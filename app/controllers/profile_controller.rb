ad177abdf88bba0fdf5349022d2e8a5b1de93034
n0b0dy-su <isrgm.00@gmail.com>
2021-05-06 19:55:45 -0500

    Users CRU
    Is needed for the users can update their own information (name, description,
    email, profile picture)
    
    I updated the `profile` controlloler to allow users to
    
    - View profile information
    - Update profile information
    - Update profile picture
    
    **There is a new issue**: can't update email beacuse `clearence` gem, I'm
    researching about it
    
    Resolves: #2

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,6 +1,28 @@
 class ProfileController < ApplicationController
+  skip_before_action :verify_authenticity_token
   before_action :require_login
 
   def show
+    @user = User.select(:id,:email,:name,:description).where(id: current_user.id)[0]
+    user = User.find(current_user.id)
+    render json: {
+      :user => @user,
+      :picture_link => url_for(user.profile_picture),
+    }
   end
+
+  def update
+    user_data = {current_user.id => profile_params}
+    User.update(user_data.keys,user_data.values)
+  end
+
+  def update_picture
+    user = User.find(current_user.id)
+    user.profile_picture.attach(params[:profile_picture])
+  end
+
+  private 
+    def profile_params
+      params.permit(:name,:email,:description,:profile_picture)
+    end
 end
