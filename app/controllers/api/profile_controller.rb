b5545a21918f9ade59edb4772b560e98ba7ec831
n0b0dy-su <isrgm.00@gmail.com>
2021-05-07 18:13:11 -0500

    Resolve confilcts and add api profile routes to name space
    When I do the first PR of the issue #2 two conflicts were found, and I saw that
    the profile routes not were in the `api name space`
    
    I've added the routes of profile api to namespace `api` and resolved confilcts
    with master branch
    
    Fixes: #2

---
+++ app/controllers/api/profile_controller.rb
@@ -0,0 +1,40 @@
+class Api::ProfileController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  before_action :require_login
+
+  def show
+    @user = User.select(:id,:email,:name,:description).where(id: current_user.id)[0]
+    user = User.find(current_user.id)
+    render json: {
+      :user => @user,
+      :picture_link => url_for(user.profile_picture),
+    }
+  end
+
+  def update
+    user_data = {current_user.id => profile_params}
+    if User.update(user_data.keys,user_data.values)
+      render json: {
+        :code => "OK"
+      }
+    else
+      head 500
+    end
+  end
+
+  def update_picture
+    user = User.find(current_user.id)
+    if user.profile_picture.attach(params[:profile_picture])
+      render json: {
+        :code => "OK"
+      }
+    else
+      head 500
+    end
+  end
+
+  private 
+    def profile_params
+      params.permit(:name,:email,:description,:profile_picture)
+    end
+end
