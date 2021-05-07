cfdbdce00b6ec7b2024bb12b2d0f859732d11559
LPMendoza <lpedromendoza333@gmail.com>
2021-05-06 20:23:32 -0500

    Exporting of CSV and PDF
    The functionality of exporting csv and pdf
    of the To Do list have been done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,33 @@ class TasksController < ApplicationController
     before_action :require_login
     def home
     end
+
+    def get_pdf 
+        @tasks = Task.select("name, status").where("user_id=#{current_user.id}").order("created_at DESC")
+        @user = current_user
+        respond_to do |format|
+            format.html
+            format.pdf do
+                render pdf: "to-do-list_#{current_user.id}_#{Time.now.strftime("%d-%m-%Y")}",
+                page_size: 'A4',
+                template: "tasks/get_pdf.html.erb",
+                layout: "pdf.html",
+                orientation: "Landscape",
+                lowquality: true,
+                zoom: 1,
+                dpi: 75
+            end
+        end
+    end
+
+    def get_csv 
+        @tasks = Task.select("name, status").where("user_id=#{current_user.id}").order("created_at DESC")
+        respond_to do |format|
+        format.html
+        format.csv { 
+            send_data @tasks.to_csv, 
+            filename: "to-do-list_#{current_user.id}_#{Time.now.strftime("%d-%m-%Y")}.csv" 
+        }
+        end
+    end
 end
